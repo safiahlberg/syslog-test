b744b341f820f6d087fb5524ef173709f8ca22e2
Safi Ahlberg <10996839+safiahlberg@users.noreply.github.com>
2022-12-13 09:55:55 +0100

    Initial rev

---
+++ src/test/java/com/wixia/syslog/SyslogTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wixia.syslog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyslogTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
